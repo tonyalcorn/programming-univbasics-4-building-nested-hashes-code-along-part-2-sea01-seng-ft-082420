9bffe14e0b9e482d4af7c8be71301f28fce46ff8
Anthony Alcorn <xmastony@gmail.com>
2020-08-16 01:47:00 +0000

    Done.

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -19,7 +19,9 @@ def update_status
          ]
       }
    }
+epic_tragedy[:montague][:hero][:status] = "dead"
 
+epic_tragedy[:capulet][:heroine][:status] = "dead"
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
 
@@ -51,7 +53,10 @@ def add_characters
          ]
       }
    }
-
+epic_tragedy[:additional_characters] = [
+  {name: "Prince Escalus"},
+  {name: "Apothecary"}
+  ]
    # Write code below that does the following programmatically:
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
